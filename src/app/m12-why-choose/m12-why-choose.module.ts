d5581fb01742b5e3e84e9d7569a2da201dbe9159
tomanasmo <tom@anasmo.no>
2023-02-24 14:18:03 +0100

    Module 12 added.

--- src/app/m12-why-choose/m12-why-choose.module.ts
+++ src/app/m12-why-choose/m12-why-choose.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { M12WhyChooseComponent } from './m12-why-choose.component';
 
-
+// Enable Material icons.
+import { MatIconModule } from '@angular/material/icon';
+const materialModules = [
+  MatIconModule
+];
 
 @NgModule({
   declarations: [
     M12WhyChooseComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [
     M12WhyChooseComponent
